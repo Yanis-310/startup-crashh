2897a7e2ddfc053a76e4542ba36885cb594042e3
Yanis-310 <taombeyanis@gmail.com>
2025-03-18 12:01:40 +0100

    init project

---
+++ Assets/script/OpenPanelButton.cs
@@ -0,0 +1,21 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class OpenPanelButton : MonoBehaviour
+// {
+// [SerializField] private PanelType type;
+
+// private MenuController controller;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         controller= FindObjectType<MenuController>();
+
+//     }
+
+// public void Onclick()
+// {
+//     controller.OpenPanel(type);
+// }
+// }
